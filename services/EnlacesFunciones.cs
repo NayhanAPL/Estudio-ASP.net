de30c1e57f19c19a6c51db017048b57c6219fe3c
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2022-02-14 01:37:06 -0500

    guardo la carpeta services

---
+++ services/EnlacesFunciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace versión_5_asp.services
+{
+    public class EnlacesFunciones: IFunciones
+    {
+        public List<string> TodasLasPosibilidades() 
+        {
+            List<string> posibilidades = new List<string>() {
+        "Crear tus propios trueques.",
+        "Recibir las sugerencias que le ofrecemos para sus trueques.",
+        "Iterar por todos los trueques que se encuentren activos para hacer su selección.",
+        "Enviar ofertas de trueques a otros usuarios.",
+        "Gestionar y mantenerce informado sobre sus ofertas enviadas.",
+        "Recibir ofertas y gestionarlas hasta su finalización.",
+        "Finalizar un TRUEQUE y comenzar la comunicación con otros usuarios.",
+        "Recibir ayuda y leer la guía sobre TRUEQUE."
+            };
+            return posibilidades;
+        }
+        
+    }
+}
---
+++ services/IFunciones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace versión_5_asp.services
+{
+    public interface IFunciones
+    {
+        List<string> TodasLasPosibilidades();
+    }
+}
