8cbf75546c61da2cae843107de67d76d247750de
Susana Becerra Pino <susanabp1992@gmail.com>
2022-02-14 23:35:39 -0500

    Trueque api controller added

---
+++ Controllers/TruequesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using versión_5_asp.Data;
+using versión_5_asp.Models;
+
+namespace versión_5_asp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TruequesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public TruequesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Trueque> Get()
+        {
+            return context.Trueques.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var trueque = context.Trueques.FirstOrDefault(x => x.Id == id);
+            if (trueque == null)
+            { 
+                return NotFound();
+            }
+            return Ok(trueque);
+        }
+    }
+}
--- Data/Migrations/20220215002148_MigracionTrueques.Designer.cs
+++ Data/Migrations/20220215002148_MigracionTrueques.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using versión_5_asp.Data;
 
 namespace versión_5_asp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220215002148_MigracionTrueques")]
+    partial class MigracionTrueques
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
