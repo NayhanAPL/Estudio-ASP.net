c5ce81a0d1a146856dbcc0cf7d73d5fa75411552
Susana Becerra Pino <susanabp1992@gmail.com>
2022-04-13 11:52:58 -0400

    Google Authentication: Created custom TruequesSettings POCO to store the client secrets

---
+++ Models/TruequesSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace versión_5_asp.Models
+{
+    public class TruequesSettings
+    {
+        public string ConnectionString { get; set; }
+        public string GoogleClientID { get; set; }
+        public string GoogleClientSecret { get; set; }
+
+    }
+}
